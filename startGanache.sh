042cf4674cb59d18782cd54d253a14d7cb03f51a
Mark Mathis <mark.mathis@alphapoint.com>
2018-07-22 10:58:16 -0400

    fixed ganace-cli

---
+++ startGanache.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Helper script for starting/stopping ganache and running tests
+#
+
+function startupGanache() {
+    GANACHE_SEARCH="$(ps -ef | grep "[g]anache" | awk '{print $2}')"
+    export GANACHE_PID="${GANACHE_SEARCH}"
+    if [ "$GANACHE_PID" != "" ]
+    then
+        echo "Killing existing Ganache CLI process $GANACHE_PID"
+        kill -9 $GANACHE_PID
+
+        ./node_modules/.bin/ganache-cli -m "$MNEMONIC" > /dev/null &
+        export GANACHE_PID=$!
+        echo "Started new Ganache CLI as process $GANACHE_PID"
+    else
+        ./node_modules/.bin/ganache-cli -m "$MNEMONIC" > /dev/null &
+        export GANACHE_PID=$!
+        echo "Started new Ganache CLI as process $GANACHE_PID"
+    fi
+}
+
+
+startupGanache
