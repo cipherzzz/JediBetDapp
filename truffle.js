9c4537d18ccef8689c2ec9ae26baa5c684897234
Mark Mathis <mark.mathis@alphapoint.com>
2018-07-23 17:31:23 -0400

    added dotenv

--- truffle.js
+++ truffle.js
@@ -1,4 +1,5 @@
-var mnemonic = "appear stomach artist ancient order chronic judge close arch sick example oxygen";
+require('dotenv').config()
+var mnemonic = process.env.MNEMONIC_PHRASE;
 var HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
@@ -6,7 +7,7 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "577" // Match any network id
+      network_id: "*" // Match any network id
     },
     ropsten: {
       provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/fYegUFu9HulgkCPiCTuy"),
